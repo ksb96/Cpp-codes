2c9bb773f1651dc5682b559d379182eb3048f5b3
kubstar <52975077+kubstar@users.noreply.github.com>
2022-11-21 16:22:41 +0530

    practice -master --commit

---
+++ hackerrank-cpp/array/sum-diff.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void sumdifference(int a, int b){
+    auto sum = a + b;
+    auto diff = a - b;
+    cout<< sum <<'\n';
+    cout << diff;
+}
+
+int main() {
+    sumdifference(4,5);
+    return 0;
+}
