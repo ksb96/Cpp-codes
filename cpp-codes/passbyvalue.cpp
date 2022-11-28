493a331eb70fcb22036896ac0eed4b13f1880b5a
kubstar <52975077+kubstar@users.noreply.github.com>
2022-11-28 17:00:48 +0530

    formatted

--- cpp-codes/passbyvalue.cpp
+++ cpp-codes/passbyvalue.cpp
@@ -1,13 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void noreturn(int x, int y){
-    cout<<x<<y<<endl;
+void noreturn(int x, int y)
+{
+    cout << x << y << endl;
 }
 
-int main() {
+int main()
+{
     int a = 5;
     int b = 7;
-    noreturn(5,7);
+    noreturn(5, 7);
     return 0;
 }
