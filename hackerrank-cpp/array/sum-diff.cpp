f7a27e185bb4e1132b88b8ddb76f5d5b4c2f8ccb
kubstar <52975077+kubstar@users.noreply.github.com>
2022-11-30 17:43:15 +0530

    exe prob --resolved

--- hackerrank-cpp/array/sum-diff.cpp
+++ hackerrank-cpp/array/sum-diff.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void sumdifference(int a, int b){
+void sumdifference(int a, int b)
+{
     auto sum = a + b;
     auto diff = a - b;
-    cout<< sum <<'\n';
+    cout << sum << '\n';
     cout << diff;
 }
 
-int main() {
-    sumdifference(4,5);
+int main()
+{
+    sumdifference(4, 5);
     return 0;
 }
