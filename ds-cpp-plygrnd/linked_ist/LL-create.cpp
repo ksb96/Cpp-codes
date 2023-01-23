234394fea1a08ebd5689c0f485afbc8aa538f4b4
kubstar <52975077+kubstar@users.noreply.github.com>
2023-01-23 13:48:35 +0530

    file restructure

--- ds-cpp-plygrnd/linked_ist/LL-create.cpp
+++ ds-cpp-plygrnd/linked_ist/LL-create.cpp
@@ -2,66 +2,71 @@
 
 using namespace std;
 
-//creating node
-class Node { 
-    public:
-        int value;
-        Node* next;
+// creating node
+class Node
+{
+public:
+    int value;
+    Node *next;
 
-        Node(int value) {
-            this->value = value;
-            next = nullptr;
-        }
-}; 
-
-//creating linked list
-class LinkedList {
-    private:
-        Node* head;
-        Node* tail;
-        int length;
-
-    public:
-        LinkedList(int value) {
-            Node* newNode = new Node(value);
-            head = newNode;
-            tail = newNode;
-            length = 1;
-        }
-
-        void printList() {
-            Node* temp = head;
-            while (temp != nullptr) {
-                cout << temp->value << endl;
-                temp = temp->next;
-            }
-        }
-
-        void getHead() {
-            cout << "Head: " << head->value << endl;
-        }
-
-        void getTail() {
-            cout << "Tail: " << tail->value << endl;
-        }
-
-        void getLength() {
-            cout << "Length: " << length << endl;
-        }
-
+    Node(int value)
+    {
+        this->value = value;
+        next = nullptr;
+    }
 };
 
+// creating linked list
+class LinkedList
+{
+private:
+    Node *head;
+    Node *tail;
+    int length;
 
+public:
+    LinkedList(int value)
+    {
+        Node *newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
 
-int main() {
-       //assigning value to the linked list(node) 
-    LinkedList* myLinkedList = new LinkedList(4);
+    void printList()
+    {
+        Node *temp = head;
+        while (temp != nullptr)
+        {
+            cout << temp->value << endl;
+            temp = temp->next;
+        }
+    }
+
+    void getHead()
+    {
+        cout << "Head: " << head->value << endl;
+    }
+
+    void getTail()
+    {
+        cout << "Tail: " << tail->value << endl;
+    }
+
+    void getLength()
+    {
+        cout << "Length: " << length << endl;
+    }
+};
+
+int main()
+{
+    // assigning value to the linked list(node)
+    LinkedList *myLinkedList = new LinkedList(4);
 
     myLinkedList->getHead();
     myLinkedList->getTail();
     myLinkedList->getLength();
-  
-    myLinkedList->printList();
-       
-}
 
+    myLinkedList->printList();
+}
