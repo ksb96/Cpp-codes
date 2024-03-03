a4c680242c8872cda703f6646c5e287ea7ea84a3
ksb96 <52975077+ksb96@users.noreply.github.com>
2024-03-03 12:15:24 +0530

    added memo-for points

--- hackerrank-cpp/array/reverse-array.cpp
+++ hackerrank-cpp/array/reverse-array.cpp
@@ -1,11 +1,13 @@
-//algo
-//reverse an array of integer(vectors)
-// [1,2,3] - input
-// [3,2,1] - output
+// algo
+// reverse an array of integer(vectors)
+//  [1,2,3] - input
+//  [3,2,1] - output
 
-vector<int> reverseArray(vector <int>a){
-    std:: vector<int> reversed;
-    for(int i = a.size()-1; i>=0; i--){
+vector<int> reverseArray(vector<int> a)
+{
+    std::vector<int> reversed;
+    for (int i = a.size() - 1; i >= 0; i--)
+    {
         reversed.push_back(a[i]);
     }
     return reversed;
